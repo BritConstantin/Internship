d05a748e32e4496116528b28f3601a850f0e3271
BritConstantine <brit111001@gmail.com>
2021-03-12 13:31:11 +0200

    fixed issue with db methods

--- PythonStudy/Hints/db_worker.py
+++ PythonStudy/Hints/db_worker.py
@@ -1,4 +1,5 @@
 import sqlite3
+from random import randint
 
 
 class DbWorker:
@@ -6,63 +7,73 @@ class DbWorker:
     def __init__(self, db_name):
         self.conn = sqlite3.connect(f'{db_name}.db')
         self.db_name = db_name
+
     # done
     def create_table(self, table_name, cols):
         c = self.conn.cursor()
         cols_str = ""
         try:
-
             for col_name in cols.keys():
-                cols_str += col_name + " " + cols[col_name]
-            c.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
-                        {col_name}
-                        )""")
+                cols_str += col_name + " " + cols[col_name] + ", "
+            cols_str = cols_str[:-2]
+            sql_command = f"""CREATE TABLE IF NOT EXISTS {table_name} (
+                        {cols_str}
+                        )"""
+            print(' executing: ' + sql_command)
+            c.execute(sql_command)
 
-            c.execute("INSERT INTO users VALUES (1, 'JawkeRz')")
 
-            # c.execute("SELECT * FROM users")
-            # print(c.fetchall())
-
-            # c.execute("INSERT INTO users VALUES (:argument_a, :argument_b)", {'argument_a': 'User1', 'argument_b': 1234})
-
-            # conn.commit()  # commit the changes to the DB
-
-            # to avoid the need to conn.commit() after every c.execute() we can use "with" statement:
-            # with self.conn:
-            #     c.execute("SELECT * FROM users")
-            # print(c.fetchall())
         except sqlite3.OperationalError as e:
-            print(self.create_table.__name__ + e)
+            print(e) # self.create_table.__name__ +
 
-    def print_table(self,table_name):
+    def drop_table(self, table_name):
         c = self.conn.cursor()
-        try:
+        sql_command = f"DROP TABLE {table_name}"
+        print(' executing: ' + sql_command)
+        c.execute(sql_command)
 
+    # done
+    def print_table(self, table_name):
+        c = self.conn.cursor()
+
+        try:
             with self.conn:
                 c.execute(f"SELECT * FROM {table_name}")
-            print(c.fetchall())
+            for s in c.fetchall():
+                print(s)
         except sqlite3.OperationalError as e:
-            print(self.create_table.__name__ + e)
+            print(self.create_table.__name__)
+            print(e)
+
+    def get_all_table_rows(self, table_name):
+        c = self.conn.cursor()
+
+        try:
+            with self.conn:
+                c.execute(f"SELECT * FROM {table_name}")
+            return c.fetchall()
+        except sqlite3.OperationalError as e:
+            print(self.create_table.__name__)
+            print(e)
+
     # fixme:
-    def insert_row(self, table_name, row):
+    def insert_row(self, table_name, column_names, row):
         c = self.conn.cursor()
         try:
+            # c.execute(f"""
+            # INSERT INTO users
+            # VALUES (1,'test_name','test_message')
+            #             """)
+            c.execute(f"""
+               INSERT INTO {table_name}
+               VALUES ({row[0]},'{row[1]}','{row[2]}')
+                           """)
+            # c.execute(f"""INSERT INTO {table_name}
+            #               VALUES ({str(column_names[0])}, {column_names[1]}, {column_names[2]}) """)
+            self.conn.commit()
 
-            c.execute("INSERT INTO users VALUES (1, 'JawkeRz')")
-
-            # c.execute("SELECT * FROM users")
-            # print(c.fetchall())
-
-            # c.execute("INSERT INTO users VALUES (:argument_a, :argument_b)", {'argument_a': 'User1', 'argument_b': 1234})
-
-            # conn.commit()  # commit the changes to the DB
-
-            # to avoid the need to conn.commit() after every c.execute() we can use "with" statement:
-            # with conn:
-            #     c.execute("SELECT * FROM users")
-            # print(c.fetchall())
         except sqlite3.OperationalError as e:
-            print(self.create_table.__name__ + e)
+            print(e)  # self.create_table.__name__ +
 
     def close_connection(self):
         self.conn.close()  # close the connection to the DB
@@ -75,9 +86,16 @@ if __name__ == '__main__':
         'message_id': 'integer',
         'user_id': 'integer',
         'message': 'string'
-    }
-    my_db = DbWorker(table_name)
-    my_db.create_table(table_name,users_hat)
+        }
+    test_row = (randint(1, 1000), "stub_name" + str(randint(1, 1000)), "stub message" + str(randint(1, 1000)))
+    my_db = DbWorker(db_name)
+    # my_db.drop_table(db_name)
+    # my_db.create_table(table_name, users_hat)
+    # my_db.print_table(table_name)
+    # print('----------------------')
+    my_db.insert_row(table_name, users_hat.keys(), test_row)
     my_db.print_table(table_name)
-    # my_db.insert_row(table_name, )
+    extracted_data = my_db.get_all_table_rows(table_name)
+    for s in extracted_data:
+        print("ed> " + s)
     my_db.close_connection()
